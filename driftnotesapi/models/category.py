c94c147476f40b50478310503263a254aedec14f
Jean Patimeteeporn <jpatimet@gmail.com>
2024-04-29 13:53:24 -0500

    fixed errors in models

--- driftnotesapi/models/category.py
+++ driftnotesapi/models/category.py
@@ -2,5 +2,4 @@ from django.db import models
 
 
 class Category(models.Model):
-
     name = models.CharField(max_length=55)
--- driftnotesapi/models/usertrip.py
+++ driftnotesapi/models/usertrip.py
@@ -3,7 +3,5 @@ from django.contrib.auth.models import User
 
 
 class UserTrip(models.Model):
-
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="usertrips")
-
     trip = models.ForeignKey("Trip", on_delete=models.CASCADE, related_name="usertrips")
