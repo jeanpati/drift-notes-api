9414c3b1c270099d112979e5fedc4169865f3ad6
Jean Patimeteeporn <jpatimet@gmail.com>
2024-04-29 14:28:14 -0500

    added user fixtures and added login/register endpoints

--- driftnotesproject/urls.py
+++ driftnotesproject/urls.py
@@ -1,10 +1,19 @@
-from django.contrib import admin
+from django.conf import settings
 from django.urls import include, path
+from django.conf.urls.static import static
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
+from driftnotesapi.models import *
+from driftnotesapi.views import *
 
 router = routers.DefaultRouter(trailing_slash=False)
 
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
-]
-
+    path("", include(router.urls)),
+    path("register", register_user),
+    path("login", login_user),
+    path("api-token-auth", obtain_auth_token),
+    path("api-auth", include("rest_framework.urls", namespace="rest_framework")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
