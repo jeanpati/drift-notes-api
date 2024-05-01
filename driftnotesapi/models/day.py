8729742c87bbda07adf8aa22637f8cb536a2fbf9
Jean Patimeteeporn <jpatimet@gmail.com>
2024-05-01 13:33:16 -0500

    edited field options in Day and Event models / Added event view

--- driftnotesapi/models/day.py
+++ driftnotesapi/models/day.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Day(models.Model):
-    trip = models.ForeignKey("Trip", on_delete=models.CASCADE, null=True)
+    trip = models.ForeignKey("Trip", on_delete=models.CASCADE)
     date = models.DateField(
         default="0000-00-00",
     )
--- driftnotesapi/models/event.py
+++ driftnotesapi/models/event.py
@@ -11,9 +11,11 @@ def event_end():
 
 
 class Event(models.Model):
-    day = models.ForeignKey("Day", on_delete=models.CASCADE, null=True)
+    day = models.ForeignKey("Day", on_delete=models.CASCADE)
     title = models.CharField(max_length=155)
-    location = models.CharField(max_length=155)
+    location = models.CharField(max_length=155, blank=True, null=True)
     start_time = models.TimeField(default=event_start)
     end_time = models.TimeField(default=event_end)
-    category = models.ForeignKey("Category", on_delete=models.DO_NOTHING, null=True)
+    category = models.ForeignKey(
+        "Category", on_delete=models.DO_NOTHING, null=True, blank=True
+    )
