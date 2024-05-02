4f0f1d13f37339ab11ced6e94bd4c97e6869d061
Jean Patimeteeporn <jpatimet@gmail.com>
2024-05-01 21:17:42 -0500

    added fixtures to script

--- seed_database.sh
+++ seed_database.sh
@@ -7,6 +7,10 @@ python3 manage.py migrate
 python3 manage.py loaddata user
 python3 manage.py loaddata category
 python3 manage.py loaddata token
+python3 manage.py loaddata day
+python3 manage.py loaddata event
+python3 manage.py loaddata trip
+python3 manage.py loaddata usertrip
 
 
 
